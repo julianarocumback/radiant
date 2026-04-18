67a5c32f7e146bd756b6be48e43502cf15d5c10b
Juliana Rocumback <julianarocumback@proton.me>
2026-04-18 20:54:58 -0300

    Ajustando conteúdo da news letter

--- src/components/newsLetter/NewsLetter.jsx
+++ src/components/newsLetter/NewsLetter.jsx
@@ -3,14 +3,16 @@ import Button from "../hero/button/Button"
 
 export default function NewsLetter(){
     return (
-        <div className="w-full h-100 flex justify-center py-20">
-            <div className="w-[70%] border border-[rgba(198,198,198,0.20)] bg-[#f3f3f5] shadow-lg rounded-4xl flex flex-col px-70 text-center justify-center gap-4">
+        <div className="w-full h-120 flex justify-center py-20">
+            <div className="w-[70%] border border-[rgba(198,198,198,0.20)] bg-[#f3f3f5] shadow-lg rounded-4xl flex flex-col px-70 text-center justify-center gap-5">
+               
                
                     <h2 className="text-3xl">Receba Gotas de Inspiração</h2>
                     <p>Inscreva-se para receber lançamentos exclusivos, reflexões semanais <br/> e descontos especiais em sua caixa de entrada.</p>
-                    <div className="flex gap-4 justify-center">
+
+                    <div className="flex py-2 gap-4 justify-center">
                         <Email/>
-                        <Button texto='Inscrever' style=' py-4 px-6 rounded-4xl w-30 font-medium bg-[#00639a] text-white' onClick=''/>
+                        <Button texto='Inscrever' style=' py-4 px-6 rounded-4xl w-30 font-medium bg-[#00639a] text-white cursor-pointer' onClick=''/>
                     </div>
 
 
