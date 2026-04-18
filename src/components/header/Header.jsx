81b7e21f97a3097eb5d8ff027a6da253e2505cc3
Juliana Rocumback <julianarocumback@proton.me>
2026-04-18 11:36:48 -0300

    Arrumando a altura do header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ import Menu from "./menu/Menu"
 
 export default function Header() {
     return(
-        <header className="flex h-10 w-all bg-white/80 justify-between px-5 items-center">
+        <header className="flex h-14 w-all bg-white/80 justify-between px-5 items-center">
             <Menu/>
             <Logo/>
             <Login/>
