1122bb0028c8e64677f2d75d0d662b842411a590
Juliana Rocumback <julianarocumback@proton.me>
2026-04-19 12:44:23 -0300

    Organizando o conteúdo da hero

--- src/components/hero/heroContent/HeroCotent.jsx
+++ src/components/hero/heroContent/HeroCotent.jsx
@@ -2,7 +2,7 @@ export default function HeroContent() {
     return (
         <div className='text-center flex flex-col gap-4 lg:text-left lg:gap-10'>
             <h1 className='hidden md:block lg:text-8xl'>A Luz que <br />Dissipa todas <br /> as Sombras.</h1>
-            <div className="flex flex-col lg:gap-2">
+            <div className="flex flex-col gap-2">
                 <p className='text-2xl/9 lg:text-1xl'><q>Lâmpada para os meus pés é a tua palavra, e luz para o meu caminho.</q></p>
                 <cite className="text-xs text-blue-500 lg:text-xl"><span className='hidden md:inline'>-</span> <span className='uppercase md:capitalize'>Salmos 119:105</span></cite>
 
