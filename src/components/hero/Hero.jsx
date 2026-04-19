e17c1a67548839ed644ce0fbf4b38e05da178558
Juliana Rocumback <julianarocumback@proton.me>
2026-04-19 12:34:07 -0300

    Organizando o conteúdo da hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero() {
         <div className='h-screen w-full flex flex-col px-10 animate-glow bg-[length:200%_200%] lg:justify-center
         bg-[radial-gradient(at_0%_0%,rgba(59,130,246,0.3),transparent_50%),radial-gradient(at_50%_0%,rgba(147,51,234,0.3),transparent_50%),radial-gradient(at_100%_0%,rgba(46,204,133,0.3),transparent_50%),radial-gradient(at_0%_100%,rgba(231,76,60,0.3),transparent_50%),radial-gradient(at_90%_100%,rgba(254,202,87,0.3),transparent_50%),radial-gradient(at_0%_100%,rgba(255,107,107,0.3),transparent_50%),radial-gradient(50%_100%,rgba(46,204,133,0.10),transparent_50%)]' >
             <div className="flex flex-col gap-10 py-16 lg:flex-row lg:justify-center lg:gap-16 lg:">
-                <div className="order-1 lg:order-0 border">
+                <div className="flex flex-col order-1 lg:order-0 gap-10">
 
                     <div className='hidden md:block'>
                         <Badge/>
@@ -19,9 +19,9 @@ export default function Hero() {
                     
                     <div className='order-1 md:order-0 flex flex-col my-1 gap-2'>
                         <HeroContent/>
-                        <div className='flex justify-center my-4'>
-                            <Button style={'bg-gradient-to-r from-[#00639A] to-[#BC004B] h-15 rounded-4xl px-8 text-white flex gap-2 items-center'} texto={`Explorar Coleção`} icone={<span className="material-icons">auto_awesome</span>}></Button>
                         </div>
+                        <div className='flex justify-center my-4 lg:justify-start order-1'>
+                            <Button style={'bg-gradient-to-r from-[#00639A] to-[#BC004B] h-15 rounded-4xl px-8 text-white flex gap-2 items-center'} texto={`Explorar Coleção`} icone={<span className="material-icons">auto_awesome</span>}></Button>
                     </div>
                 
                 </div>
--- src/components/hero/heroContent/HeroCotent.jsx
+++ src/components/hero/heroContent/HeroCotent.jsx
@@ -1,9 +1,9 @@
 export default function HeroContent() {
     return (
-        <div className='text-center flex flex-col gap-4'>
-            <h1 className='hidden md:block'>A Luz que Dissipa todas as Sombras.</h1>
-            <p className='text-2xl/9'><q>Lâmpada para os meus pés é a tua palavra, e luz para o meu caminho.</q></p>
-            <cite className="text-xs text-blue-500"><span className='hidden md:inline'>-</span> <span className='uppercase md:capitalize'>Salmos 119:105</span></cite>
+        <div className='text-center flex flex-col gap-4 lg:text-left'>
+            <h1 className='hidden md:block lg:text-8xl'>A Luz que <br />Dissipa todas <br /> as Sombras.</h1>
+            <p className='text-2xl/9 lg:text-1xl'><q>Lâmpada para os meus pés é a tua palavra, e luz para o meu caminho.</q></p>
+            <cite className="text-xs text-blue-500 lg:text-xl"><span className='hidden md:inline'>-</span> <span className='uppercase md:capitalize'>Salmos 119:105</span></cite>
         </div>
     )
 }
